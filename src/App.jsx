93a4bfbe7de62b1a60cb18b2c8e6285b05e2a43c
wince <winceogola20@gmail.com>
2026-02-17 23:14:28 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+function App(){
+  return (
+    <div className = "mt-10 text-center">
+      <h1 className = "text-5xl font-bold text--800">Welcome to VoxLedger</h1>
+    <Button>
+      Test
+      </Button>
+      </div>
+  )
+}
+export default App
