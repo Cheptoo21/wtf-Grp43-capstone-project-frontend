dc980e4212142448e97d86e6e351e734ac34259b
Olaleye Oluwatomide <paulinaebunoluwa@gmail.com>
2026-02-24 23:07:33 +0100

    Voice setup page completed

--- src/pages/VoiceSetup.jsx
+++ src/pages/VoiceSetup.jsx
@@ -1,9 +1,13 @@
+import VoiceSetupLayout from "@/components/UserSetup/VoiceSetupLayout";
 import UserHeader from "../components/UserSetup/UserHeader";
+
+
 
 export default function VoiceSetup() {
     return (
         <>
             <UserHeader />
+            <VoiceSetupLayout />
         </>
     )
 
