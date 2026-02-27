62a6603e2ac1d7c5572c910c67dad41515b4184c
Cheptoo Gitonga <cheptoopatience81@gmail.com>
2026-02-27 22:21:19 +0300

    Fix imports and case-sensitive paths for Render

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { Routes, Route } from "react-router-dom"
-import Home from "./pages/Home"
-import Signup from "./pages/Signup"
-import Login from "./pages/login"
-import ProfileSetup from "./pages/ProfileSetup"
-import VoiceSetup from "./pages/VoiceSetup"
-import Dashboard from "./pages/dashboard"
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Signup from "./pages/signup";
+import Login from "./pages/login";
+import ProfileSetup from "./pages/ProfileSetup";
+import VoiceSetup from "./pages/VoiceSetup";
+import Dashboard from "./pages/dashboard";
 
 export default function App() {
   return (
@@ -14,7 +14,7 @@ export default function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/profile-setup" element={<ProfileSetup />} />
       <Route path="/voice-setup" element={<VoiceSetup />} />
-       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
-  )
+  );
 }
